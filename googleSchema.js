f98266d33c880ff4c27d1cf1cf226db13faab99a
Ionita Andrei <62765799+dazetoyo@users.noreply.github.com>
2021-03-17 02:20:36 +0100

    Fix docusaurus window error

--- googleSchema.js
+++ googleSchema.js
@@ -33,12 +33,12 @@ string += iterateString;
 string += `]}`;
 
 // inject string into markdown
-window.onload = function(){
+document.addEventListener("DOMContentLoaded", function(event){
 	const elementExists = document.getElementById("philosophy-faq");
 	if (elementExists) {
 		document.querySelector("#philosophy-faq").innerHTML = string;
 	}
-};
+});
 
 
 //TODO add recurring interval when switching location href, find a better way than calling an interval every 500 ms
