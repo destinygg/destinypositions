93db203612958512b6fb7c9eb6a7a1ed32c5a1f5
Ionita Andrei <62765799+dazetoyo@users.noreply.github.com>
2021-03-21 23:16:17 +0100

    rewrite the filter function, rewrite the json initialize, add docitem component by mattroseman

--- googleSchema.js
+++ googleSchema.js
@@ -1,33 +1,42 @@
 // Philosophy FAQ
-// to add more philosophy questions just add them in this json as the first 2 and the script will do the rest
-
-const faqPhilosophy = {
-	"faqs": {
-		"protectingOurDemographics": {
-			"question": "What does Destiny think about protecting our demographics?",
-			"answer": "Destiny believes that this endeavor is futile because he rejects the notion that there is a consistent and coherent definition of American values",
-		},
-		"protestingRioting": {
-			"question": "What does Destiny think about the protesting and rioting?",
-			"answer": "Destiny ardently believes in people's right to protest and will always support the right to protest, even if he doesn't necessarily agree with the positions being advocated for by any particular protest. ",
-		},
+// to add more philosophy questions just add them in this string as the first 2 and the script will do the rest
+const faqPhilosophy = `{
+	"protectingOurDemographics":{
+		"question":"What does Destiny think about protecting our demographics?",
+		"answer":"Destiny believes that this endeavor is futile because he rejects the notion that there is a consistent and coherent definition of American values"
+	},
+	"protestingRioting":{
+		"question":"What does Destiny think about the protesting and rioting?",
+		"answer":"Destiny ardently believes in people's right to protest and will always support the right to protest, even if he doesn't necessarily agree with the positions being advocated for by any particular protest."
 	}
-};
+}`;
 
 // inject string into markdown
 if (typeof window !== 'undefined') {
 	 function createSchema () {
-		// Initialize JSON array and data
-		let faqsPhilosophy = faqPhilosophy.faqs;
-		faqsPhilosophy = JSON.stringify(faqsPhilosophy);
-		faqsPhilosophy = JSON.parse(faqsPhilosophy);
+		// Initialize JSON data, replace removes the spaces from the string so it can be parsed by JSON.Parse
+		let faqsPhilosophy = JSON.parse(faqPhilosophy.replace(' ', ''));
+
+		// function to iterate over the JSON
+		const iterateSchemaFunction = (obj) => {
+		    Object.keys(obj).forEach(key => {
+				if (key === 'question' ) {
+					iterateSchemaString +=`{"@type": "Question","name": "` + obj[key] + `",`;
+				}
+				if (key === 'answer') {
+						iterateSchemaString +=`"acceptedAnswer": {"@type": "Answer","text": "` + obj[key] + `"}},`;
+				}
+		    if (typeof obj[key] === 'object') {
+		            iterateSchemaFunction(obj[key])
+		        }
+		    })
+		}
+
 		let schemaString = `{"@context": "https://schema.org","@type": "FAQPage","mainEntity":[`;
 			let iterateSchemaString = '';
 			// iterates over the questions
-			Object.values(faqsPhilosophy).filter(faqsPhilosophy =>
-				iterateSchemaString += `{"@type": "Question","name": "` + faqsPhilosophy.question + `","acceptedAnswer": {"@type": "Answer","text": "` + faqsPhilosophy.answer + `"}},`
-			);
-			//slices last ','
+			iterateSchemaFunction(faqsPhilosophy);
+			// slices last ','
 			iterateSchemaString = iterateSchemaString.slice(0, -1);
 			schemaString += iterateSchemaString;
 			schemaString += `]}`;
@@ -37,24 +46,5 @@ if (typeof window !== 'undefined') {
 
 		createSchema();
 
-		const schemaString = createSchema();
-
-		//checks if the script element is present on the page
-		window.onload = function(){
-			const elementExists = document.getElementById("philosophy-faq");
-			if (elementExists) {
-				document.querySelector("#philosophy-faq").innerHTML = schemaString;
-			}
-		};
-
-		//checks if the client has changed link
-		let schemaPushState = history.pushState;
-		history.pushState = function () {
-			schemaPushState.apply(history, arguments);
-			const elementExists = document.getElementById("philosophy-faq");
-			if (elementExists) {
-				document.querySelector("#philosophy-faq").innerHTML = schemaString;
-			}
-		};
-
+		window.schemaStringFAQ = createSchema();
 	}
